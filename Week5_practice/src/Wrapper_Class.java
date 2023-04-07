8c2bb3e01001789936e9b5c00b90e5d2a18a2d46
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2023-04-07 12:48:26 +0900

    Wrapper Class

---
+++ Week5_practice/src/Wrapper_Class.java
@@ -0,0 +1,18 @@
+public class Wrapper_Class {
+   /* int myNum = 42;
+    Integer myInteger = new Integer(myNum);
+    System.out.println(myInteger.toString());*/
+
+    /*double myDouble = 3.14;
+    Double myWrapper = new Double(myDouble);
+    System.out.println(myWrapper.toString());*/
+
+    /*boolean myBool = true;
+    Boolean myWrapper = new Boolean(myBool);
+    System.out.println(myWrapper.toString());*/
+
+    //Java Wrapper Classes allows us to convert primitive data types into objects.
+    // Integer, Double and Boolean are wrapper classes for int, double and boolean respectively.
+
+
+}
