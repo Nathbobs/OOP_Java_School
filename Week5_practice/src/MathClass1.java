3d2d08c1fa5cbb13ef7aa74416dcb4936441a531
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2023-04-07 10:37:19 +0900

    Math Class

---
+++ Week5_practice/src/MathClass1.java
@@ -0,0 +1,36 @@
+import static java.lang.Math.pow;
+
+public class MathClass1 {
+    public static double pow(double base, double exponent) { //"pow" is a method in the Math class used to calculate the power of a number
+        return pow(base, exponent);
+    }
+    public static double abs(double num) { //"abs" is a method in the Math class used to calculate the absolute value of a number
+        return num;
+    }
+    public static double sqrt(double num) { //"sqrt" is a method in the Math class used to calculate the square root of a number
+        return num;
+    }
+    public static double cbrt(double num) { //"cbrt" is a method in the Math class used to calculate the cube root of a number
+        return num;
+    }
+    public static double ceil(double num) { //"ceil" is a method in the Math class used to calculate the ceiling of a number(returns the smallest whole number that is greater than or equal to the number)
+        return num;
+    }
+    public static double floor(double num) { //"floor" is a method in the Math class used to calculate the floor of a number(returns the largest integer that is less than or equal to the number)
+        return num;
+    }
+    public static double round(double num) { //"round" is a method in the Math class used to calculate the rounded value of a number
+        return num;
+    }
+    public static double max(double num1, double num2) { //"max" is a method in the Math class used to calculate the maximum value of two numbers
+        return num1;
+    }
+    public static double min(double num1, double num2) { //"min" is a method in the Math class used to calculate the minimum value of two numbers
+        return num1;
+    }
+    public static double random() { //"random" is a method in the Math class used to calculate a random number between 0 and 1
+        return 0;
+    }
+    double num = Math.random(); //returns a random number between 0 and 1
+    double num2 = Math.random() * 10; //returns a random number between 0 and 10
+}
