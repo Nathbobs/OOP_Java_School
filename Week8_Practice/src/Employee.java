66dd163c2ed28ca2813f101bffe241d5f070baf8
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2023-05-04 11:45:03 +0900

    Wee9 Lab

--- Week8_Practice/src/Employee.java
+++ Week8_Practice/src/Employee.java
@@ -1,2 +1,11 @@
+import java.util.Date;
+
 public class Employee {
+    private String name;
+    private Date hireDate;
+
+    public Employee(){
+        name = "No name";
+        hireDate = new Date("January", 1, 1000); // Just a placeholder.
+    }
 }
